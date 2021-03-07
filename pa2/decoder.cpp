0dc21da525691413bad7f2f22c5d69096678f44b
EthanLin1688 <80032259+EthanLin1688@users.noreply.github.com>
2021-03-07 08:26:31 -0800

    Update decoder.cpp

--- pa2/decoder.cpp
+++ pa2/decoder.cpp
@@ -1,6 +1,7 @@
 #include "decoder.h"
 #include "queue.h"
 #include "stack.h"
+#include "treasureMap.h"
 using namespace std;
 
 decoder::decoder(const PNG & tm, pair<int,int> s)
@@ -12,18 +13,18 @@ decoder::decoder(const PNG & tm, pair<int,int> s)
 
 PNG decoder::renderSolution(){
 
-	for(int i = 0; i < pathPts.size(); i++){
-		RGBAPixel *pixel = mapImg.getPixel(pathPts[i].first, pathPts[i].second);
-		pixel->r = 255;
+    for(int i = 0; i < pathPts.size(); i++){
+        RGBAPixel *pixel = mapImg.getPixel(pathPts[i].first, pathPts[i].second);
+        pixel->r = 255;
         pixel->g = 0;
         pixel->b = 0;
-	}
+    }
 
 }
 
 PNG decoder::renderMaze(){
 
-	PNG copy = mapImg;
+    PNG copy = mapImg;
     queue<pair<int, int>> locations;
 
     for(int i = start.first-3; i =< start.first+3; i++){
@@ -57,39 +58,69 @@ PNG decoder::renderMaze(){
 
 }
 
+
 void decoder::setGrey(PNG & im, pair<int,int> loc){
 
-/* YOUR CODE HERE */
-
+    int x = loc.first;
+    int y = loc.second;
+    RGBAPixel *pixel = im.getPixel(x,y);
+    pixel->r = 2*((pixel->r)/4);
+    pixel->g = 2*((pixel->g)/4);
+    pixel->b = 2*((pixel->b)/4);
 }
 
 pair<int,int> decoder::findSpot(){
-
-/* YOUR CODE HERE */
-
+    return pathPts[pathPts.size()-1];
 }
 
 int decoder::pathLength(){
-
-/* YOUR CODE HERE */
-
+    return pathPts.size()-1;
 }
 
 bool decoder::good(vector<vector<bool>> & v, vector<vector<int>> & d, pair<int,int> curr, pair<int,int> next){
-
-/* YOUR CODE HERE */
-
+    if(next.first >= base.width() || next.second >= base.height() || next.first < 0 || next.second < 0){
+        return false;
+    } else if (v[next.first][next.second] == true){
+        return false;
+    } else {
+        RGBAPixel *p_next = maze.getPixel(next.first,next.second);
+        dis = d[curr.second][curr.first];
+        return compare(p_next, dis);
+    }
 }
 
 vector<pair<int,int>> decoder::neighbors(pair<int,int> curr) {
 
-/* YOUR CODE HERE */
+    vector<pair<int,int>> n;
+    int x = curr.first;
+    int y = curr.second;
+    n.push_back(pair<int,int>(x-1,y));
+    n.push_back(pair<int,int>(x,y+1));
+    n.push_back(pair<int,int>(x+1,y));
+    n.push_back(pair<int,int>(x,y-1));
+
+    return n;
 
 }
 
 
 bool decoder::compare(RGBAPixel p, int d){
+    vector<int> bin_r = treasureMap.ten_2(p.r,6);
+    vector<int> bin_g = treasureMap.ten_2(p.g,6);
+    vector<int> bin_b = treasureMap.ten_2(p.b,6);
 
-/* YOUR CODE HERE */
+    vector<int> final;
+    final.push_back(bin_r[4]);
+    final.push_back(bin_r[5]);
+    final.push_back(bin_g[4]);
+    final.push_back(bin_g[5]);
+    final.push_back(bin_b[4]);
+    final.push_back(bin_b[5]);
 
+    int final_i = treasureMap.two_10(final);
+    if(final_i == 0 && d == 63){
+        return true;
+    }else{
+        return(final_i == d+1);
+    }
 }
